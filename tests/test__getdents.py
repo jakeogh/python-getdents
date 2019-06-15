2098a63c14b52e0c5d5e10181512b52501e3dd15
Justin Keogh <github.com@v6y.net>
2019-06-14 18:53:26 -0700

    auto-commit

--- tests/test__getdents.py
+++ tests/test__getdents.py
@@ -22,12 +22,12 @@ def fixt_regular_file(tmpdir):
 
     os.close(fd)
 
-    tmpdir = os.fsencode(tmpdir)
+    #tmpdir = os.fsencode(tmpdir)
     print(tmpdir)
     all_valid_bytes = set([bytes(chr(x), encoding='Latin-1') for x in range(0, 256)]) - set([b'\x00', b'\x2F'])
     for b in all_valid_bytes:
         print(b)
-        f = tmpdir + b'/' + b
+        f = tmpdir / b
         f.write('content')
         fd = os.open(str(f), os.O_RDONLY)
         yield fd
