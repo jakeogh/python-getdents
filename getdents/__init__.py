3af2f6dc2146a8a5bd0fdc809ee206e6f5c849ce
jakeogh <github.com@v6y.net>
2019-07-16 00:20:04 -0700

    auto-commit

--- getdents/__init__.py
+++ getdents/__init__.py
@@ -47,10 +47,9 @@ def getdents(path, buff_size=32768, verbose=False):
         #            yield (inode, dtype, name)
         yield from (
             (inode, type, name)
-            for inode, type, name in getdents_raw(fd, buff_size)
+            for inode, type, name in getdents_raw(fd, 32768*2)
             if not(type == DT_UNKNOWN or inode == 0 or name in (b'.', b'..'))
         )
-
     finally:
         os.close(fd)
 
