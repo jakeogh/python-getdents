54577f01ec76b628eb1d88c72d7e410ca94fc85b
jakeogh <github.com@v6y.net>
2019-11-13 03:41:57 -0700

    auto-commit

--- getdents/__init__.py
+++ getdents/__init__.py
@@ -173,6 +173,7 @@ class DentGen():
             elif dent.is_dir():
                 self.path = dent.parent + b'/' + dent.name
                 if cur_depth < self.max_depth:
+                    print(cur_depth, "<", self.max_depth)
                     yield from self.__iter__(cur_depth + 1)
                 elif cur_depth == self.max_depth:
                     yield dent
