2d727d78c26ab1d1eb6140de03ecf63e2566a2ec
jakeogh <github.com@v6y.net>
2019-11-13 20:46:56 -0700

    auto-commit

--- getdents/__init__.py
+++ getdents/__init__.py
@@ -150,11 +150,11 @@ class Dent():
 
 @attr.s(auto_attribs=True)
 class DentGen():
-    min_depth: int
     max_depth: float = inf
     path: bytes = attr.ib(converter=os.fsencode)
     buff_size: int = BUFF_SIZE
     verbose: bool = False
+    min_depth: int
     #iters: int = 0
 
     def __attrs_post_init__(self):
