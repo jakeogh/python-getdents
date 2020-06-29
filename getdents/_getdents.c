58157c72558185c710ea9855b6d6d97154770c56
jakeogh <github.com@v6y.net>
2020-06-28 22:14:47 -0700

    auto-commit

--- getdents/_getdents.c
+++ getdents/_getdents.c
@@ -188,7 +188,9 @@ getdents_next(struct getdents_state *s)
         idx = 0;
         for (idx=0; idx<=index; idx++) {
             fprintf(stderr, "%d %lu\n", idx, random_dents[idx]);
-            //struct linux_dirent64 *dd = (struct linux_dirent64 *)(s->buff + );
+            struct linux_dirent64 *dd = (struct linux_dirent64 *)(random_dents[idx]);
+            fprintf(stderr, "%lu %hu dd->name: %s\n", random_dents[idx], dd->d_reclen, dd->d_name);
+
         }
 
         free(buff);
