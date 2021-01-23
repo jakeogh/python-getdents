d3e1072784407990a28c29bf0b1ae77d79af27d5
jakeogh <github.com@v6y.net>
2021-01-23 16:17:11 -0700

    auto-commit

--- getdents/__init__.py
+++ getdents/__init__.py
@@ -387,7 +387,7 @@ def links(path,
           max_depth=inf,
           min_depth=0,
           random: bool = False,) -> Generator:
-    print("links():", path, file=sys.stderr)
+    #print("links():", path, file=sys.stderr)
     return paths(path=path,
                  return_dirs=False,
                  return_symlinks=True,
