303a49bd7660048d89286180ac915fcb08164e50
jakeogh <github.com@v6y.net>
2021-04-05 17:59:32 -0700

    auto-commit

--- getdents/__init__.py
+++ getdents/__init__.py
@@ -11,19 +11,26 @@ from typing import Sequence
 
 import attr
 
+from ._getdents import \
+    DT_BLK  # noqa: ignore=F401 # pylint: disable=import-error
+from ._getdents import \
+    DT_CHR  # noqa: ignore=F401 # pylint: disable=import-error
+from ._getdents import \
+    DT_DIR  # noqa: ignore=F401 # pylint: disable=import-error
+from ._getdents import \
+    DT_FIFO  # noqa: ignore=F401 # pylint: disable=import-error
+from ._getdents import \
+    DT_LNK  # noqa: ignore=F401 # pylint: disable=import-error
+from ._getdents import \
+    DT_REG  # noqa: ignore=F401 # pylint: disable=import-error
+from ._getdents import \
+    DT_SOCK  # noqa: ignore=F401 # pylint: disable=import-error
+from ._getdents import \
+    DT_UNKNOWN  # noqa: ignore=F401 # pylint: disable=import-error
+from ._getdents import \
+    MIN_GETDENTS_BUFF_SIZE  # noqa: ignore=F401 # pylint: disable=import-error
 from ._getdents import O_GETDENTS  # pylint: disable=import-error
 from ._getdents import getdents_raw  # pylint: disable=import-error
-
-#from ._getdents import DT_BLK  # noqa: ignore=F401
-#from ._getdents import DT_CHR  # noqa: ignore=E0401
-#from ._getdents import DT_DIR
-#from ._getdents import DT_FIFO
-#from ._getdents import DT_LNK
-#from ._getdents import DT_REG
-#from ._getdents import DT_SOCK
-#from ._getdents import DT_UNKNOWN
-#from ._getdents import MIN_GETDENTS_BUFF_SIZE
-
 
 BUFF_SIZE = 4096 * 16  # 64k
 
