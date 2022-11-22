cfbbe7f0ac6f73a5a8647e51912aee6d00746272
jakeogh <github.com@v6y.net>
2022-11-22 01:10:54 -0700

    auto-commit

--- getdents/__init__.py
+++ getdents/__init__.py
@@ -141,10 +141,10 @@ class Dent:
             self.parent = b"/".join(split_p[:-1])
             # del split_p
         self.path = b"/".join((self.parent, self.name))
-        assert Path(os.fsdecode(self.path)).exists()
         eprint(
             f"Dent      __init__() {self.path=!r} {self.name=} {split_p=} {self.parent=!r} {self.path=!r}"
         )
+        assert Path(os.fsdecode(self.path)).exists()
         # self.pathlib = Path(os.fsdecode(self.path))
         self.lstat = None
 
