2b2c85a84e97991a8612975a19cc52e32432ae85
jakeogh <github.com@v6y.net>
2022-11-22 12:52:36 -0700

    auto-commit

--- getdents/__init__.py
+++ getdents/__init__.py
@@ -423,7 +423,7 @@ class DentGen:
                     ic("yielding", dent)
                     yield dent
                 ic("setting", self.path, "to", dent.parent)
-                assert False
+                #assert False
                 self.path = dent.parent
             else:
 
